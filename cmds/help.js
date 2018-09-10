8078ac7e1ae2c1751760a706084ae05f7808173f
kby101 <41835437+kby101@users.noreply.github.com>
2018-09-10 07:35:09 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -10,7 +10,7 @@ exports.run = async (bot, message, args) => {
   .setTitle("Hello", bicon)
   .setDescription(`Thanks for using me, my prefix is \`\`${config.prefix}\`\` and my music command is :`)
   .addField('♫ Music', '\`play\`, \`skip\`, \`pause\`, \`np\`, \`resume\`, \`queue\`, \`leave\`')
-  .addField('✎ Info', '\`ping\`, \`botinfo\`, \`stats\`')
+  .addField('✎ Info', '\`ping\`, \`stats\`')
   .addField('Support', '\`bugreport\`, \`botinvite\`')
   .addField("**Link**", "[Invite me](https://discordapp.com/oauth2/authorize?client_id=471757779091193856&scope=bot&permissions=1664478529) | [Support Server](https://discord.gg/JPUa7KP)")
   .setTimestamp()
